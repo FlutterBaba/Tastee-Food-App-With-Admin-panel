4d79fd94232688bfd7b4dcf6e5a39fb1632e0584
Yaqoob Bugti <yaqoobbugti580@gmail.com>
2021-08-13 14:32:03 +0500

    home page  done

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:tasteefood/appColors/app_colors.dart';
 import 'package:tasteefood/widgets/build_drawer.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,8 +20,167 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Center(
-        child: Text("HomePage"),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
+              elevation: 7,
+              shadowColor: Colors.grey[300],
+              child: TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  fillColor: AppColors.KwhiteColor,
+                  hintText: "Search Your Product",
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Text(
+              "Categories",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey[600],
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Categories(
+                  categoryName: "Food",
+                  image: "images/logo.jpg",
+                ),
+                Categories(
+                  categoryName: "burger",
+                  image: "images/logo.jpg",
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Text(
+              "Products",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SingleProduct(),
+                SingleProduct(),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Text(
+              "Best Sell",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SingleProduct(),
+                SingleProduct(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SingleProduct extends StatelessWidget {
+  const SingleProduct({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.all(12.0),
+          height: 200,
+          width: 150,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "\$30",
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "Copea flour",
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class Categories extends StatelessWidget {
+  final String image;
+  final String categoryName;
+
+  const Categories({
+    Key? key,
+    required this.categoryName,
+    required this.image,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(12.0),
+      height: 100,
+      width: 150,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            image,
+          ),
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(categoryName),
       ),
     );
   }
